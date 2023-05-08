77057f846042e5b4fb87f16709eabb3fab2de75a
Bouchra <hb_benbarek@esi.dz>
2023-05-08 21:27:10 +0200

    login function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,52 @@
 import React from 'react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../style/style.css';
 import logo from '../assets/images/logo.svg';
 import Home from './Home.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash, faSlash } from '@fortawesome/free-solid-svg-icons';
+import propTypes from 'prop-types';
 
-const Login = () =>{
+async function loginUser(credentials){
+  return fetch('http://localhost:8081/login', {
+    method:'POST',
+    headers : {
+      'Content-Type':'application/json',
+    },
+    body: JSON.stringify(credentials)
+  })
+  .then(data=>data.json())
+}
 
+const Login = ({setToken}) =>{
+  const navigate = useNavigate();
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [email, setEmail] = useState('');
   const [pwd, setPwd] = useState({password : "", showPassword : false});
 
-  const login=(e)=>{
+  // const login=(e)=>{
+  //   e.preventDefault();
+  //   const data = {
+  //     email, 
+  //     pwd
+  //   }
+  //   localStorage.setItem('token-info', JSON.stringify(data));
+  //   setIsLoggedin(true);
+  //   setEmail('');
+  //   setPwd('');
+  // }
+  
+  const handleSubmit = async e =>{
     e.preventDefault();
-    const data = {
-      email, 
+    const token = await loginUser({
+      email,
       pwd
-    }
-    localStorage.setItem('token-info', JSON.stringify(data));
-    setIsLoggedin(true);
-    setEmail('');
-    setPwd('');
+    })
+    setToken=token;
+    navigate("/home");
   }
+    
 
   const handlePasswordChange = (prop) =>(event) =>{
     setPwd({...pwd, [prop]: event.target.value});
@@ -41,7 +65,7 @@ const Login = () =>{
           <img src={logo} alt="logo" style={{height:'100px' }} />
         </div>
          <h1>Login to continue</h1>
-         <form className="form ui">
+         <form className="form ui" onSubmit={handleSubmit}>
            <div className="field">
             <input type ="email" className="email" placeholder="Email address" value={email} onChange={(e)=>setEmail(e.target.value)}/>
             <span className='line'></span>
@@ -54,7 +78,7 @@ const Login = () =>{
             <span className='line'></span>
             <a href='#' className='pwd-forgotten'>Password forgotten?</a>
            </div>
-           <button className="submit-btn" type="submit" onClick={login}>Login</button>
+           <button className="submit-btn" type="submit">Login</button>
            <div className="signup" >
              Don't have an account? 
            <a href='#'>Sign up</a>
@@ -70,5 +94,9 @@ const Login = () =>{
     
   </div> 
   );
+}
+
+Login.prototypes = {
+  setToken : propTypes.func.isRequired
 }
 export default Login;
