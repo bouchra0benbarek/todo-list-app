ff2faa2c01782c0e8dac81062d1e4396e17ac9e0
Bouchra <hb_benbarek@esi.dz>
2023-05-08 21:26:46 +0200

    Add routes + handeling tokens

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Login from './components/Login.js';
 import Home from './components/Home.js';
 import { useState } from 'react';
+import useToken from './useToken';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
- 
-  return (
-    <div className="App">
-      {/* <Login/> */}
-     <Home/>
-    </div>
-  );
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }else{
+    return (
+      <div className="App">
+          <Routes>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/home' element={<Home/>}/>
+          </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
